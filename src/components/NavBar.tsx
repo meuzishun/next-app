709b0d1d36ce610e9fb533a8eedadbc9a162877e
Andrew Smith <meuzishun@gmail.com>
2025-11-10 17:01:53 -0500

    Create NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+export function NavBar() {
+  return (
+    <nav className='py-5 px-5 text-2xl'>
+      <ul className='flex justify-end gap-5'>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/list'>List</Link>
+        </li>
+        <li>
+          <Link href='/map'>Map</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
