cda727f09da248a34a6bdd84d024033328612cbb
Andrew Smith <meuzishun@gmail.com>
2025-11-19 17:22:01 -0500

    Reformat Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,13 @@ import Link from 'next/link';
 function Logo() {
   return (
     <Link href="/">
-      <Image src="/images/logo.png" alt="Logo" width={100} height={100} />
+      <Image
+        src="/images/logo.png"
+        alt="Logo"
+        width={100}
+        height={100}
+        loading="eager"
+      />
     </Link>
   );
 }
