30033318bdc79ee5d057cecdbfd18d0a28981194
Andrew Smith <meuzishun@gmail.com>
2025-11-21 17:40:43 -0500

    Ignore ts issue with geo

--- src/app/map/Map.tsx
+++ src/app/map/Map.tsx
@@ -4,6 +4,7 @@ import { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useCountryStats } from '@/hooks/useCountryStats';
+// @ts-ignore
 import geo from 'countries-cities-geo';
 
 export function getCountryCoords(countryCode) {
