5e322ae444a1bbd77e4649149a377917e1245eb0
Andrew Smith <meuzishun@gmail.com>
2025-11-30 15:36:07 -0500

    Add getRoleCountsForCountry to chingu controller

--- src/features/chingu/chingu.controller.ts
+++ src/features/chingu/chingu.controller.ts
@@ -3,6 +3,7 @@ import {
   ChinguQueryOptions,
   ChinguType,
   ChinguCountryStats,
+  ChinguCountryRoleStats,
 } from './chingu.type';
 
 function mapError(e: unknown): Error {
@@ -30,6 +31,21 @@ export const chinguController = {
   async countryStats(): Promise<ChinguCountryStats[]> {
     try {
       return await chinguService.getCountsByCountry();
+    } catch (e) {
+      throw mapError(e);
+    }
+  },
+
+  async getRoleCountsForCountry(params: {
+    countryCode?: string;
+    countryName?: string;
+  }): Promise<ChinguCountryRoleStats> {
+    try {
+      if (!params.countryCode && !params.countryName) {
+        throw new Error('Either countryCode or countryName is required');
+      }
+
+      return await chinguService.getRoleCountsForCountry(params);
     } catch (e) {
       throw mapError(e);
     }
