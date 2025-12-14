f2b284fd7152e160f4deeb6abed6663836e64860
Andrew Smith <meuzishun@gmail.com>
2025-12-13 21:03:52 -0500

    Adjust styles in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 
 function Header() {
   return (
-    <header className="flex flex-row justify-between align-center py-3 pl-3 pr-5 z-20 bg-white">
+    <header className="bg-chingu-green-600 flex flex-row justify-between items-center py-3 pl-3 pr-5 border-b border-chingu-green-400 z-20">
       <Logo />
       <MobileNav />
       <Navbar />
