4b55e1e9f9888b4f2f675651ea2fab0bae772e15
Andrew Smith <meuzishun@gmail.com>
2025-12-13 19:10:24 -0500

    Move map settings into effect hook

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -27,101 +27,101 @@ export const Map = () => {
   const animationFrameIdRef = useRef<number | null>(null);
   const lastTimeRef = useRef(0);
 
-  const mapStyle: StyleSpecification = {
-    version: 8,
-    name: 'My Custom Style',
-    glyphs: 'mapbox://fonts/mapbox/{fontstack}/{range}.pbf',
-    sprite: 'mapbox://sprites/mapbox/streets-v12',
-    fog: {
-      color: '#2e74a3',
-      'high-color': '#b64fff',
-      'space-color': [
-        'interpolate',
-        ['linear'],
-        ['zoom'],
-        0,
-        '#010b19',
-        4,
-        '#010b19',
-        7,
-        '#367ab9',
-      ],
-      'horizon-blend': 0.1,
-      'star-intensity': 0.6,
-      range: [
-        'interpolate',
-        ['linear'],
-        ['zoom'],
-        0,
-        ['literal', [0.2, 1]],
-        22,
-        ['literal', [0.2, 1]],
-      ],
-    },
-    sources: {
-      'mapbox-streets': {
-        type: 'vector',
-        url: 'mapbox://mapbox.mapbox-streets-v8',
-      },
-      'mapbox-countries': {
-        type: 'vector',
-        url: 'mapbox://mapbox.country-boundaries-v1',
-      },
-      'country-labels-source': {
-        type: 'vector',
-        url: 'mapbox://mapbox.mapbox-streets-v8',
-      },
-    },
-    layers: [
-      {
-        id: 'background',
-        type: 'background',
-        paint: {
-          'background-color': '#171e3d',
-          'background-pattern': 'dots-large',
-        },
-      },
-      {
-        id: 'water',
-        type: 'fill',
-        source: 'mapbox-streets',
-        'source-layer': 'water',
-        paint: {
-          'fill-color': '#1b2f52',
-        },
-      },
-      {
-        id: 'country-boundaries',
-        type: 'fill',
-        source: 'mapbox-countries',
-        'source-layer': 'country_boundaries',
-        paint: {
-          'fill-color': '#171e3d',
-          'fill-outline-color': '#40b8f5',
-        },
-      },
-      {
-        id: 'country-labels',
-        type: 'symbol',
-        source: 'country-labels-source',
-        'source-layer': 'place_label',
-        filter: ['==', ['get', 'type'], 'country'],
-        layout: {
-          'text-field': ['get', 'name_en'],
-          'text-size': 12,
-        },
-        paint: {
-          'text-color': '#40b8f5',
-          'text-halo-color': '#171e3d',
-          'text-halo-width': 1,
-        },
-        minzoom: 1,
-      },
-    ],
-  };
-
   useEffect(() => {
     if (!mapContainerRef.current) return;
+
+    const mapStyle: StyleSpecification = {
+      version: 8,
+      name: 'My Custom Style',
+      glyphs: 'mapbox://fonts/mapbox/{fontstack}/{range}.pbf',
+      sprite: 'mapbox://sprites/mapbox/streets-v12',
+      fog: {
+        color: '#2e74a3',
+        'high-color': '#b64fff',
+        'space-color': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          0,
+          '#010b19',
+          4,
+          '#010b19',
+          7,
+          '#367ab9',
+        ],
+        'horizon-blend': 0.1,
+        'star-intensity': 0.6,
+        range: [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          0,
+          ['literal', [0.2, 1]],
+          22,
+          ['literal', [0.2, 1]],
+        ],
+      },
+      sources: {
+        'mapbox-streets': {
+          type: 'vector',
+          url: 'mapbox://mapbox.mapbox-streets-v8',
+        },
+        'mapbox-countries': {
+          type: 'vector',
+          url: 'mapbox://mapbox.country-boundaries-v1',
+        },
+        'country-labels-source': {
+          type: 'vector',
+          url: 'mapbox://mapbox.mapbox-streets-v8',
+        },
+      },
+      layers: [
+        {
+          id: 'background',
+          type: 'background',
+          paint: {
+            'background-color': '#171e3d',
+            'background-pattern': 'dots-large',
+          },
+        },
+        {
+          id: 'water',
+          type: 'fill',
+          source: 'mapbox-streets',
+          'source-layer': 'water',
+          paint: {
+            'fill-color': '#1b2f52',
+          },
+        },
+        {
+          id: 'country-boundaries',
+          type: 'fill',
+          source: 'mapbox-countries',
+          'source-layer': 'country_boundaries',
+          paint: {
+            'fill-color': '#171e3d',
+            'fill-outline-color': '#40b8f5',
+          },
+        },
+        {
+          id: 'country-labels',
+          type: 'symbol',
+          source: 'country-labels-source',
+          'source-layer': 'place_label',
+          filter: ['==', ['get', 'type'], 'country'],
+          layout: {
+            'text-field': ['get', 'name_en'],
+            'text-size': 12,
+          },
+          paint: {
+            'text-color': '#40b8f5',
+            'text-halo-color': '#171e3d',
+            'text-halo-width': 1,
+          },
+          minzoom: 1,
+        },
+      ],
+    };
 
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN as string;
     const m = new mapboxgl.Map({
