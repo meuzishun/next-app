b880d8670a0817c98eb079a4ec14364b56e70be0
Andrew Smith <meuzishun@gmail.com>
2025-12-13 23:43:21 -0500

    Move onclick to div

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,14 +6,16 @@ function Logo() {
   const { showHomeView } = useUIView();
 
   return (
-    <div className="flex flex-row justify-center items-center gap-2">
+    <div
+      className="flex flex-row justify-center items-center gap-2"
+      onClick={showHomeView}
+    >
       <Image
         src="/images/logo.webp"
         alt="Logo"
         width={32}
         height={32}
         loading="eager"
-        onClick={showHomeView}
       />
       <p className="font-semibold text-2xl text-white">Chingu</p>
     </div>
